8b3a18c826a99548a105c20519a3e70c583b3bbf
sturmenta <sturmenta@gmail.com>
2021-11-07 05:59:33 -0300

    choose mp3 files & reproduce 🔥

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,46 @@
 import React from "react";
-import logo from "./logo.svg";
+import { Howl } from "howler";
+
 import "./App.css";
 
 function App() {
+  const handleSongFiles = async (event: any) => {
+    if (event.target.files.length !== 0) {
+      const files = event.target.files;
+      const songsPath: string[] = [];
+
+      for (let x = 0; x < files.length; x++) {
+        await new Promise((res: any) => {
+          const fl = new FileReader();
+          fl.addEventListener("load", (loadEvent: any) => {
+            songsPath.push(loadEvent.target.result);
+            res();
+          });
+          fl.readAsDataURL(files[x]);
+        });
+      }
+
+      const autoplay = (i: number, list: any) => {
+        const sound = new Howl({
+          src: [list[i]],
+          preload: true,
+          onend: () => autoplay(i + 1 === list.length ? 0 : i + 1, list),
+        });
+        sound.play();
+      };
+      autoplay(0, songsPath);
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <input
+          type="file"
+          onChange={handleSongFiles}
+          accept=".mp3, .wav, .flac"
+          multiple
+        />
       </header>
     </div>
   );
